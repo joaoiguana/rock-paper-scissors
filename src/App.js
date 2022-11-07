d4dd5455600743db84d9543080c9b62363fa68c3
joao garcia <jiboiadamor@gmail.com>
2022-11-07 21:07:50 +0000

    created react rock-paper-scissors app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { FaHandRock } from 'react-icons/fa';
+import { FaHandPaper } from 'react-icons/fa';
+import { FaHandScissors } from 'react-icons/fa';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center'>
+      <h1>Rock Paper Scissors</h1>
+      <div>
+        <div className='container'>
+          <div className='player'>
+            <div className='score'>Player 1: 0</div>
+            <div></div>
+          </div>
+          <div className='player'>
+            <div className='score'>Player 2: 0</div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <button className='round-bnt'>
+            <FaHandRock size={20} />
+          </button>
+          <button className='round-bnt'>
+            <FaHandPaper size={20} />
+          </button>
+          <button className='round-bnt'>
+            <FaHandScissors size={20} />
+          </button>
+        </div>
+        <h2>Player 1 wins!</h2>
+      </div>
     </div>
   );
 }
