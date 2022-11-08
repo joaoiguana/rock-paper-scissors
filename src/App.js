720128e15c9aa08d86f234d2690b0a2e612a608f
joao garcia <jiboiadamor@gmail.com>
2022-11-08 10:49:23 +0000

    create action on click to select action

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import { FaHandRock } from 'react-icons/fa';
-import { FaHandPaper } from 'react-icons/fa';
-import { FaHandScissors } from 'react-icons/fa';
+import { FaHandRock, FaHandPaper, FaHandScissors } from 'react-icons/fa';
 import './App.css';
+import {useState} from 'react';
 
-function Player({name = "Player", score = 0}) {
+function ActionIcon({action, ...props}) {
+  const icons = {
+    rock: FaHandRock,
+    paper: FaHandPaper,
+    scissors: FaHandScissors,
+  };
+  const Icon = icons[action]
+  return (<Icon {...props}/>);
+}
+
+function Player({name = "Player", score = 0, action = "rock"}) {
   return (
     <div className='player'>
       <div className='score'>{name}: {score}</div>
       <div className='action'>
-        <FaHandRock size={60} />
+        {action && <ActionIcon action={action} size={60} />}
       </div>
     </div>
   )
 }
 
+function ActionButton({action = "rock", onActionSelected}) {
+  return (
+    <button className='round-bnt' onClick={() => onActionSelected(action)}>
+      <ActionIcon action={action} size={20} />
+    </button>
+  )
+}
+
 function App() {
+  const [playerAction, setPlayerAction] = useState("")
+
+  const onActionSelected = (selectedAction) => {
+    setPlayerAction(selectedAction)
+  }
+
   return (
     <div className='center'>
       <h1>Rock Paper Scissors</h1>
       <div>
         <div className='container'>
-          <Player name="Player" score={0} />
-          <Player name="Computer" score={0} />
+          <Player name="Player" score={0} action={playerAction} />
+          <Player name="Computer" score={0} action="paper" />
         </div>
         <div>
-          <button className='round-bnt'>
-            <FaHandRock size={20} />
-          </button>
-          <button className='round-bnt'>
-            <FaHandPaper size={20} />
-          </button>
-          <button className='round-bnt'>
-            <FaHandScissors size={20} />
-          </button>
+          <ActionButton action="rock" onActionSelected={onActionSelected} />
+          <ActionButton action="paper" onActionSelected={onActionSelected} />
+          <ActionButton action="scissors" onActionSelected={onActionSelected} />
         </div>
         <h2>Player 1 wins!</h2>
       </div>
